a7e0313306f684763087f10f8e8d5059a2ba2db8
Nikita Zelikov <zelaniki@gmail.com>
2020-12-16 16:58:22 +0300

    '6/10'

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  
+  if (Array.isArray(members)) {
+    let res = [];
+    for (let i = 0; i < members.length; i++) {
+      if (typeof(members[i]) === 'string') {
+        let currentName = members[i];
+        for (let j = 0; j < currentName.length; j++) {
+          if (currentName[j] !== ' ') {
+            res.push(currentName[j].toUpperCase());
+            break;
+          } else continue;
+        } 
+      } else continue;
+    }
+    return res.sort().join('');
+  } else return false;
 };
