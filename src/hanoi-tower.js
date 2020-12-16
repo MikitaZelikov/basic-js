ba05af3484eb899868cce13a2ceb5afa90abd191
Nikita Zelikov <zelaniki@gmail.com>
2020-12-16 16:59:03 +0300

    '6/10'

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let result = {};
+  let x = Math.pow(2, disksNumber) - 1;
+  let y = Math.floor(3600 / turnsSpeed * x);
+
+  result.turns = x;
+  result.seconds = y;
+  return result;
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+ 
+  if (Array.isArray(arr)) {
+
+    let resArr = arr.slice();
+
+    for (let i = 0; i < resArr.length; i++) {
+      if (resArr[i] === '--discard-next' && (resArr[i+2] === '--discard-prev' || resArr[i+2] === '--double-prev')) {
+        resArr.splice(i, 3);
+        i--;
+      }
+      if (resArr[i] === '--discard-next' && resArr.length > i + 1) {
+        resArr.splice(i, 2);
+        i--;
+      }
+      if (resArr[i] === '--discard-next' && resArr.length === i + 1) {
+        resArr.splice(i, 1);
+      }
+      if (resArr[i] === '--discard-prev' && i !== 0) {
+        resArr.splice(i - 1, 2);
+        i--;
+      }
+      if (resArr[i] === '--discard-prev' && i === 0) {
+        resArr.splice(i, 1);
+      }
+      if (resArr[i] === '--double-next' && resArr.length > i + 1) {
+        resArr.splice(i, 1, resArr[i + 1]);
+      }
+      if (resArr[i] === '--double-next' && resArr.length === i + 1) {
+        resArr.splice(i, 1);
+      }
+      if (resArr[i] === '--double-prev' && i !== 0) {
+        resArr.splice(i, 1, resArr[i - 1]);
+      }
+      if (resArr[i] === '--double-prev' && i === 0) {
+        resArr.splice(i, 1);
+      }
+    }
+    return resArr;
+
+  } else throw new Error();
+
 };
