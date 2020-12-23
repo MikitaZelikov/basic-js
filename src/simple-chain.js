630f9f14e6f4a98200800d41d4e774d0875d21df
Nikita Zelikov <zelaniki@gmail.com>
2020-12-23 14:11:44 +0300

    7/10

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  arr: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value === undefined) {
+      this.arr.push('');
+      return this;
+    } else if (value === null) {
+      this.arr.push(value);
+      return this;
+    } else {
+      this.arr.push(value.toString());
+      return this;
+    }
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof(position) === "number" && position % 1 == 0 && this.arr.length >= position && position > 0) {
+      this.arr.splice(position - 1, 1);
+      return this;
+    } else {
+      this.arr = [];
+      throw new Error('Error');
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let res = this.arr.map(x => `( ${x} )`).join('~~');
+    this.arr = [];
+    return res;
   }
 };
 
--- src/what-season.js
+++ src/what-season.js
@@ -2,7 +2,7 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
     if (date !== undefined) {
-        if (typeof(date) === "object" && Date.parse(date) !== NaN) {
+        if (typeof(date) === "object" && Date.parse(date) !== NaN && date.getTime(date) - Date.parse(date) !== 0) {
             if (date.getMonth() > 1 && date.getMonth() < 5) return 'spring'; 
             if (date.getMonth() > 4 && date.getMonth() < 8) return 'summer';
             if (date.getMonth() > 7 && date.getMonth() < 11) {
@@ -11,5 +11,3 @@ module.exports = function getSeason(date) {
         } else throw new Error();
     } else return 'Unable to determine the time of year!';
 };
-
-// && date === null)
